8881e19b8b77665169254e9bec334cf11c93844e
wangrufan-frank <wangrufan1011@gmail.com>
2026-07-08 18:50:31 +0800

    feat: add monthly learning metadata config (data.js)

---
+++ meiyueyixue/data.js
@@ -0,0 +1,14 @@
+// meiyueyixue/data.js
+var monthlyData = {
+  current: "2026-07",
+  articles: {
+    "2026-07": {
+      theme: "车辆盘查战术要点",
+      summary: "本月经典案例复盘 + 最新执法规范解读",
+      author: "巡防百科编辑部",
+      tags: ["盘查", "车辆", "战术"],
+      image: "",
+      file: "2026-07.html"
+    }
+  }
+};
